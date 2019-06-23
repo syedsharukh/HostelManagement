6b0ad3eac178c04d796a6156aa4b17505c9149a1
abolisetty <abolisetty@tollplus.com>
2019-06-23 17:29:42 +0530

    added InsertTenantDetails

--- DotNetCore/HostelManagementService/HostelManagementService/BLL/TenantBLL.cs
+++ DotNetCore/HostelManagementService/HostelManagementService/BLL/TenantBLL.cs
@@ -2,6 +2,8 @@
 using HostelManagementService.Models;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
+using System;
+
 namespace HostelManagementService.BLL
 {
     public class TenantBLL
@@ -14,6 +16,11 @@ namespace HostelManagementService.BLL
             IList<Tenant> tenants = new List<Tenant>();
             tenants = new TenantDAL().GetAllTenantDetails();
             return tenants;
+        }
+
+        internal bool InsertTenantDetails(Tenant tenant)
+        {
+            return new TenantDAL().InsertTenantDetails(tenant);
         }
     }
 }
